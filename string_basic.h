1f325b42376ea475842ff675a5b9b9802dfeae24
senbc <152078445+senbc@users.noreply.github.com>
2023-12-27 19:42:05 +0100

    Update string_basic.h

--- string_basic.h
+++ string_basic.h
@@ -1,14 +1,13 @@
 #pragma once
-#pragma warning(disable : 4996)
-
+#pragma warning(disable : 4996) // disables warning for strncpy on MSVC
 #include <cstring>
 #include <iostream>
 
-constexpr int STRING_BUFFER{256};
+constexpr int STRING_BUFFER{ 256 };
 
 class string_basic {
 private:
-	char m_string[256] = {" "};
+	char m_string[STRING_BUFFER] = {" "};
 public:
 	string_basic() {}
 
@@ -17,6 +16,8 @@ public:
 		m_string[sizeof(m_string) - 1] = '\0'; 
 	}
 
+	~string_basic() {}
+
 	friend std::ostream& operator<<(std::ostream& out, const string_basic& string);
 	friend std::istream& operator>>(std::istream& in, string_basic& string);
 
@@ -24,3 +25,14 @@ public:
 		return m_string;
 	}
 };
+
+std::ostream& operator<<(std::ostream& out, const string_basic& string) {
+	out << string.get_string();
+
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, string_basic& string) {
+	in.getline(string.m_string, sizeof(string.m_string));
+	return in;
+}
